00a4aaf6ee1a4c0aa4e01f65e758b2b98590159d
David Wells <wellsland@gmail.com>
2014-02-06 22:58:57 -0800

    Created Xcode Project

---
+++ 15 Puzzle/15 Puzzle/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  15 Puzzle
+//
+//  Created by David Wells on 2/6/14.
+//  Copyright (c) 2014 David Wells. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
