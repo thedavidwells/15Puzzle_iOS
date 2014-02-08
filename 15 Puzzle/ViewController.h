0a69ab7c88f048448ff1eb5d9c80e2b330c17958
David Wells <wellsland@gmail.com>
2014-02-07 23:07:35 -0800

    Added model files, and added properties for each of the 15 buttons.

--- 15 Puzzle/ViewController.h
+++ 15 Puzzle/ViewController.h
@@ -10,4 +10,23 @@
 
 @interface ViewController : UIViewController
 
+
+@property (weak, nonatomic) IBOutlet UIButton *button1;
+@property (weak, nonatomic) IBOutlet UIButton *button2;
+@property (weak, nonatomic) IBOutlet UIButton *button3;
+@property (weak, nonatomic) IBOutlet UIButton *button4;
+@property (weak, nonatomic) IBOutlet UIButton *button5;
+@property (weak, nonatomic) IBOutlet UIButton *button6;
+@property (weak, nonatomic) IBOutlet UIButton *button7;
+@property (weak, nonatomic) IBOutlet UIButton *button8;
+@property (weak, nonatomic) IBOutlet UIButton *button9;
+@property (weak, nonatomic) IBOutlet UIButton *button10;
+@property (weak, nonatomic) IBOutlet UIButton *button11;
+@property (weak, nonatomic) IBOutlet UIButton *button12;
+@property (weak, nonatomic) IBOutlet UIButton *button13;
+@property (weak, nonatomic) IBOutlet UIButton *button14;
+@property (weak, nonatomic) IBOutlet UIButton *button15;
+
+
+
 @end
---
+++ 15 Puzzle/gameModel.m
@@ -0,0 +1,13 @@
+//  15 PUZZLE
+//  PROJECT 2
+//  CS470 - Spring 2014
+//
+//  CREATED BY DAVID WELLS
+//  Copyright (c) 2014 David Wells. All rights reserved.
+//  All code not provided by Professor Kooshesh is the sole work of David Wells for CS470 at Sonoma State University.
+
+#import "gameModel.h"
+
+@implementation gameModel
+
+@end
